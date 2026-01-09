82f20bb0518b17d7d9aaf55672bc84bdb26458b2
Budka <budbayrsukhbat8@gmail.com>
2026-01-09 12:05:15 +0800

    session05

---
+++ month01/week01/session05/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+      // variable array
+      int [] myNumbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; //
+      // index : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
+      
+
+
+
+        System.out.println(num1);
+        System.out.println(myNumbers[5]);
+    }
+    
+} 
+  
