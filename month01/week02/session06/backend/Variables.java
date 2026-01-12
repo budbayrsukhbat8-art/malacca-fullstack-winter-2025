83338b75972c9e052f62af7ca5e080d0aa7dcf6d
Budka <budbayrsukhbat8@gmail.com>
2026-01-12 12:01:03 +0800

    session_06 add

---
+++ month01/week02/session06/backend/Variables.java
@@ -0,0 +1,42 @@
+
+
+public class Variables {
+    public static void main(String[] args) {
+        String myName = "Budbayar"; // text huwisagch
+      System.out.println(myName);
+      
+      byte smallNumber = 100; //
+      System.out.println(smallNumber); // -127 <-> 127
+      short mediumNumber = 30000;
+      System.out.println(mediumNumber); // -32,768 to 32,767
+       
+      int age = 25;
+      System.out.println(age); // -2 billion to 2 billion
+
+       long bigNumber = 123456789L;
+       System.out.println(bigNumber); // mash tom too (L suffix)
+       // Decimal types: butarhai too
+       float price = 99.99f ;
+       System.out.println(price);
+       
+       double pi = 3.14159265359;
+       System.out.println(pi); // 15 oron
+       
+       // Other types busad torluud 
+
+       char grade = 'A';
+       System.out.println(grade);
+
+       String name = "Bold";
+       System.out.println(name);
+       boolean isStudent = true; // false 
+       System.out.println(isStudent);
+
+       // variable name rules
+       // zowshoorogdson
+       // useg, too, _, $ aguulsan huwisagchiin nertei
+       String studentName_1$ = "Budka";
+
+    }
+    
+}
